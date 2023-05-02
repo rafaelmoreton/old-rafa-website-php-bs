6a76d819ac27644c24b5ecb1085ab5ab0f0abe47
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2023-05-02 14:40:11 -0300

    Add whole projects text content

--- locale/en.php
+++ locale/en.php
@@ -1,5 +1,11 @@
 <?php
     $langArray = array(
         "chess-project-description" => "This is a chess game written fully in Ruby to be played in the command-line interface by two local players. It enforces all the expected rules of chess and allows games to be saved and resumed later.",
+        "flight-project-description" => "The app allows users to select desired dates and airports, presents them a list of corresponding available flights, allows them to select the desired flight and enter passenger informations for all passengers.",
+        "events-project-description" => "This site allows users to create and manage events. Anyone can see available events, but only signed in users may confirm attendance to them and create new ones.",
+        "sketch-project-description" => "A browser pixel drawing tool. It is entirely front-end: JavaScript used to manipulate the page's DOM elements and CSS properties accordingly to mouse input event listeners.",
+        "connect-project-description" => "A Connect Four game to be played between two local players in the command line. It was entirely developed with a TDD approach using the Rspec gem. The app enforces the game rules and recognizes the end of match conditions.",
+        "dashboard-project-description" => "This page is mainly a CSS grid practice. I cloned this widespread dashboard design by eye. It's a single static page - the grid layout and responsiveness learning were the main point of this project.",
+        "calculator-project-description" => "A JavaScript browser calculator. It supports the four basic arithmetic operations (add, subtract, multiply and divide) and can be used with a either a mouse or a keyboard.",
     );
 ?>
--- locale/pt.php
+++ locale/pt.php
@@ -1,5 +1,11 @@
 <?php
     $langArray = array(
         "chess-project-description" => "Um jogo de xadrez escrito em Ruby para ser jogado em uma interface de linha de comando por dois jogadores locais. Ele aplica todas as regras esperadas do xadrez e permite que partidas sejam salvas e depois continuadas.",
+        "flight-project-description" => "O aplicativo permite que os usuários selecionem datas e aeroportos, apresenta-lhes uma lista de vôos correspondentes, permite-lhes selecionar o vôo desejado e inserir as informações de cada passageiro.",
+        "events-project-description" => "Esse site permite aos usuários criar e administrar eventos. Qualquer um pode ver quais eventos estão disponíveis, mas apenas usuários logados podem confirmar presença neles e criar novos.",
+        "sketch-project-description" => "Uma ferramenta de desenho em píxeis no navegador. É inteiramente front-end: JavaScript usado para manipular elementos do DOM da página e propriedades CSS de conforme o mouse interage com event listeners.",
+        "connect-project-description" => "Um jogo de 'Conecte Quatro' para a linha de comando. Ele foi inteiramente deselvolvido com uma abordagem TDD usando a gem Rspec. O aplicativo aplica as regras e reconhece as condições de fim de jogo.",
+        "dashboard-project-description" => "Essa página é principalmente um treino de CSS grid. Eu clonei de olho esse design de painel de controle amplamente disseminado. É uma página estática única - o aprendizado de layout em grid e responsividade foram o ponto principal desse projeto.",
+        "calculator-project-description" => "Uma calculadora de navegador em JavaScript. Ela suporta as quatro operações aritméticas básicas (adição, subtração, multiplicação e divisão) e pode ser usada com tanto um mouse quanto um teclado.",
     );
 ?>
