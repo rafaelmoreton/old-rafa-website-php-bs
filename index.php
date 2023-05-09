99dbdf7b7e45ef398aec371158ecb6a2aadf08ed
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2023-05-09 20:40:40 -0300

    Layout 'contact' section for small screens

--- index.php
+++ index.php
@@ -412,36 +412,17 @@
           <?php echo $langArray['contact'] ;?>
         </h2>
         <div>
-          <div>
+          <a href="mailto:rafaelmoreton@gmail.com"
+          class="d-flex gap-2 align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg"
               width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
               stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
               <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
               <polyline points="22,6 12,13 2,6"></polyline>
             </svg>
-            <p>rafaelmoreton@gmail.com</p>
-          </div>
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg"
-              width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-              stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-              <circle cx="12" cy="10" r="3"></circle>
-            </svg>
-            <p>
-              <?php echo $langArray['address'] ;?>
-            </p>
-          </div>
-            <a href="mailto:rafaelmoreton@gmail.com">
-              <p>Email</p>
-              <svg xmlns="http://www.w3.org/2000/svg"
-                width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                <polyline points="22,6 12,13 2,6"></polyline>
-            </svg>
-            </a>
-            <a href="https://github.com/rafaelmoreton">
+            <p class="m-0">rafaelmoreton@gmail.com</p>
+          </a>
+            <!-- <a href="https://github.com/rafaelmoreton">
               <p>Github</p>
               <svg xmlns="http://www.w3.org/2000/svg"
                 width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -450,17 +431,30 @@
                 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5
                 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
             </svg>
-            </a>
-            <a href="https://linkedin.com/in/rafael-moreton-baggio-3709b1217">
-              <p>LinkedIn</p>
-              <svg xmlns="http://www.w3.org/2000/svg"
-                width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
-                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                <rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle>
-              </svg>
-            </a>
+            </a> -->
+          <a href="https://linkedin.com/in/rafael-moreton-baggio-3709b1217"
+          class="d-flex gap-2 align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg"
+              width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+              <rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle>
+            </svg>
+            <p class="m-0">LinkedIn</p>
+          </a>
+          <div class="d-flex gap-2 align-items-center justify-content-end">
+           <p class="m-0" style="font-size: 0.9em;">
+              <?php echo $langArray['address'] ;?>
+            </p>
+            <svg xmlns="http://www.w3.org/2000/svg"
+              width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
+              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+              <circle cx="12" cy="10" r="3"></circle>
+            </svg>
+          </div>
         </div>
+        <p class="mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia cum necessitatibus delectus nisi aliquam possimus, officia corrupti? Distinctio, ab inventore. Voluptate aut sapiente vel! Nobis ratione hic enim amet corporis.</p>
       </div>
     </section>
 
